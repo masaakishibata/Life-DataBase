2a343ad54b9d51dacbe23ed7899d95f97be6a18b
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2021-01-30 22:50:27 +0900

    レイアウト修正6

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   root to: 'users#index'
   resources :users, only: [:create, :index, :new, :show, :edit, :destroy, :update]
+  resource :actions, only: [:new, :create, :index, :edid, :destroy]
+  resource :scores, only: [:new, :create, :index, :edid, :destroy]
 
 end
--- src/db/migrate/20210118135647_create_scores.rb
+++ src/db/migrate/20210118135647_create_scores.rb
@@ -1,9 +1,9 @@
 class CreateScores < ActiveRecord::Migration[6.1]
   def change
     create_table :scores do |t|
-      t.date        :score_month,        null: false
-      t.date        :score_day,          null: false
-      t.references  :user,                   foreign_key: true
+      t.integer        :score_month,            null: false
+      t.integer        :score_day,              null: false
+      t.references     :user,                   foreign_key: true
       t.timestamps
     end
   end
