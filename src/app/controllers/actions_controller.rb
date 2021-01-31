024422b1cd880edd42c758c3c79b59c1026199f1
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2021-01-31 16:04:28 +0900

    controllers修正

--- src/app/controllers/actions_controller.rb
+++ src/app/controllers/actions_controller.rb
@@ -7,13 +7,48 @@ class ActionsController < ApplicationController
     @action = Action.new
   end
 
+  def edit
+    @action = Action.find(params[:id])
+  end
+
   def create
+    @action = Actions.new(action_params)
+    if @action.valid?
+      @action.save
+      redirect_to '/users/:id'
+    else
+      render action: :index
+    end
   end
 
   def show
   end
 
   def destroy
+    if user_signed_in? && @action.user_id == current_user.id
+      @action.destroy
+      redirect_to root_path
+    else
+      render root_path
+    end
   end
 
+  def update
+    @action = Action.find(params[:id])
+    @action.update(action_params)
+    redirect_to '/users/:id'
+  end
+
+
+  private
+
+  def action_params
+    params.require(:actions).permit(
+      :living_expenses,
+      :food_expenses,
+      :shopping,
+    ).merge(
+      user_id: current_user.id,
+    )
+  end
 end
--- src/app/controllers/scores_controller.rb
+++ src/app/controllers/scores_controller.rb
@@ -7,12 +7,46 @@ class ScoresController < ApplicationController
     @score = Score.new
   end
 
+  def edit
+    @score = Score.find(params[:id])
+  end
+
   def create
+    @score = Scores.new(score_params)
+    if @score.valid?
+      @score.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   def show
   end
 
   def destroy
+    if user_signed_in? && @score.user_id == current_user.id
+      @score.destroy
+      redirect_to root_path
+    else
+      render root_path
+    end
+  end
+
+  def update
+    @score = Scores.find(params[:id])
+    @score.update(score_params)
+    redirect_to '/users/:id'
+  end
+
+  private
+
+  def score_params
+    params.require(:scores).permit(
+      :score_month,
+      :score_day,
+    ).merge(
+      user_id: current_user.id,
+    )
   end
 end
