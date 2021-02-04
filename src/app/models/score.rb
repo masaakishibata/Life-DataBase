36d35412487bd828c508ad5a11acd98326b11fa6
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2021-02-04 23:36:21 +0900

    scores view作成

--- src/app/models/score.rb
+++ src/app/models/score.rb
@@ -3,6 +3,7 @@ class Score < ApplicationRecord
   with_options presence: true do
     validates :score_month
     validates :score_day
+    validates :score_year
   end
 
   belongs_to :users
--- src/db/migrate/20210118135647_create_scores.rb
+++ src/db/migrate/20210118135647_create_scores.rb
@@ -1,6 +1,7 @@
 class CreateScores < ActiveRecord::Migration[6.1]
   def change
     create_table :scores do |t|
+      t.integer        :score_year,             null: false
       t.integer        :score_month,            null: false
       t.integer        :score_day,              null: false
       t.references     :user,                   foreign_key: true
