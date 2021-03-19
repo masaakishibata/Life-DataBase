23bd877949d91826de701cb2629404304f966cb8
masaakishibata <godchiwawa@icloud.com>
2021-03-19 23:18:21 +0900

    aeets add

---
+++ src/app/assets/config/manifest.js
@@ -0,0 +1,9 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+// public/assets/config/manifest.js
+//
+//= link application.css
+//= link marketing.css
+//
+//= link application.js
