2c43106a27fd205daa28142f23356b5d62bee8ba
masaakishibata <godchiwawa@icloud.com>
2021-03-22 22:52:46 +0900

    reset1

--- src/app/assets/config/manifest.js
+++ src/app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
+//= link_directory ../javascript .js
 //= link_directory ../stylesheets .css
-// public/assets/config/manifest.js
+// assets/config/manifest.js
 //
 //= link application.css
 //= link marketing.css
